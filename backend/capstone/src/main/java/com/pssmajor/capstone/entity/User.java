2d7bc72353b6df747c1d0f45f1dd1badfbf05dfd
yasatr <Yash.Atrey.protiviti.co.in>
2023-10-13 18:32:11 +0530

    User entity

--- backend/capstone/src/main/java/com/pssmajor/capstone/entity/User.java
+++ backend/capstone/src/main/java/com/pssmajor/capstone/entity/User.java
@@ -1,5 +1,39 @@
 package com.pssmajor.capstone.entity;
 
-public class User {
+import java.time.Instant;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String phoneNo;
+	private String role;
+	private String profilePicUrl;
+	@UpdateTimestamp
+	private Instant modifiedAt;
+	private Boolean isActive = false;
 }
