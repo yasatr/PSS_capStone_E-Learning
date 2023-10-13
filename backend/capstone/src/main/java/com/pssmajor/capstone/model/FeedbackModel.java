fe331483a8306fa8ebed9e6f2988738e0e79aeee
MusKau01 <Muskan.Kaushal@protiviti.co.in>
2023-10-13 19:00:13 +0530

    imported feedback

--- backend/capstone/src/main/java/com/pssmajor/capstone/model/FeedbackModel.java
+++ backend/capstone/src/main/java/com/pssmajor/capstone/model/FeedbackModel.java
@@ -1,5 +1,11 @@
 package com.pssmajor.capstone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
