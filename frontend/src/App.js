27878cffbdbe58474d3e4ae705ee148e5fff7266
ayuman01 <ayush.mangla@protiviti.co.in>
2023-11-29 18:24:06 +0530

    Added course card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 
-import { ChakraBaseProvider, Heading} from '@chakra-ui/react';
+import { ChakraProvider, Heading} from '@chakra-ui/react';
 import './App.css';
+import CourseCard from './Components/Teacher/CourseCard';
+
 
 function App() {
   return (
-    <ChakraBaseProvider>
+    <ChakraProvider>
       <Heading>Hello</Heading>
-    </ChakraBaseProvider>
+    </ChakraProvider>
   );
 }
 
