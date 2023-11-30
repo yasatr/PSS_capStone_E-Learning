34c0c68d248955a866b1c45fa2d26fc74524dde3
yasatr <Yash.Atrey.protiviti.co.in>
2023-11-30 17:30:48 +0530

    AddCourse Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,36 @@
+import { ChakraProvider, extendTheme} from '@chakra-ui/react';
+import './App.css';
+import Profile from './Pages/Teacher/Profile';
 
-// import { ChakraProvider, Heading, extendTheme} from '@chakra-ui/react';
-// import './App.css';
-// import Navbar from './Components/NavBar/Navbar';
-// import AllCourses from './Pages/Student/AllCourses';
-// import CourseCard from './Pages/Teacher/CourseCard';
-
-// function App() {
-//   const colors = {
-//     brand: {
-//       50: "#ecefff",
-//       100: "#cbceeb",
-//       200: "#a9aed6",
-//       300: "#888ec5",
-//       400: "#666db3",
-//       500: "#4d5499",
-//       600: "#3c4178",
-//       700: "#2a2f57",
-//       800: "#181c37",
-//       900: "#080819"
-//     }
-//   };
-//   const config = {
-//     initialColorMode: "dark",
-//     useSystemColorMode: false
-//   };
+function App() {
+  const colors = {
+    brand: {
+      50: "#ecefff",
+      100: "#cbceeb",
+      200: "#a9aed6",
+      300: "#888ec5",
+      400: "#666db3",
+      500: "#4d5499",
+      600: "#3c4178",
+      700: "#2a2f57",
+      800: "#181c37",
+      900: "#080819"
+    }
+  };
+  const config = {
+    initialColorMode: "dark",
+    useSystemColorMode: false
+  };
   
-//   const theme = extendTheme({ colors, config });
+  const theme = extendTheme({ colors, config });
   
-//   return (
-//     <ChakraProvider theme={theme}>
-//       <Navbar/>
-//       <Heading>Hello</Heading>
-//     </ChakraProvider>
-//   );
-// }
+  return (
+    <ChakraProvider theme={theme}>
+      <Profile/>
+    </ChakraProvider>
+  );
+}
 
-// export default App;
+export default App;
 
 
--- frontend/src/Pages/Teacher/Profile.jsx
+++ frontend/src/Pages/Teacher/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../../Components/NavBar/Navbar';
+import AddCourse from './AddCourse';
+import Footer from '../../Components/Footer/Footer';
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div>
+      <Navbar/>
+      <AddCourse/>
+      <Footer/>
+    </div>
   )
 }
 
